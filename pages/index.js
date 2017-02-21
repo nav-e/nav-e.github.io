dbde952222f9dfe656baec80ca4c53d03407aa0b
Max Lorenz <maxlorenz@users.noreply.github.com>
2017-02-21 13:31:00 +0100

    Add files via upload (#21)
    * Add files via upload
    
    * Added a landing page in Markdown
    
    * updated the landing page text

--- pages/index.js
+++ pages/index.js
@@ -43,55 +43,26 @@ const Index = () => (
 
       <div style={{ background: "#ECEFF1" }}>
         <section style={STYLES.section}>
-          <h1>The Community</h1>
+          <h1>About Green Navigation</h1>
           <p>
-            The GreenNav organization is a community of students and young professionals working on navigation ideas
-            for sustainable mobility. We love to collaborate in the open source environment
-            by prototyping new ideas and showing our results to the public.
+            Green Navigation (also: GreenNav) is an open source organization that emerged from an university project at the University of Lübeck, but is now independent. Our organization’s aim is to find the most energy efficient route for electric vehicles while providing a framework for new routing approaches.
           </p>
           <p>
-            The projects of the GreenNav organization are closely related to the student projects at the
-            university's computer science program. However, with this organisation we invite everyone to
-            participate in the development of experimental routing systems.
-          </p>
-          <p>
-            Because the GreenNav organization is not funded, the contributions are mostly done by
-            volunteers.
-            The <a
-              href="http://www.isp.uni-luebeck.de/staff/schoenfr"
-            >project manager <i className="fa fa-external-link" /></a> is a PhD student researching on routing problems
-            for sustainable transportation and his scholarship
-            is funded by
-            the <a
-              href="http://www.eksh.org/"
-            >Gesellschaft für Energie und Klimaschutz Schleswig Holstein (EKSH) <i className="fa fa-external-link" /> </a>.
-            However, most of the work is done by students willing to work on the project during their studies.
+            Even though there are existing routing services out there, we aim to support sustainable means of transportation. None of the other routing services considers aspects such as a prediction of the energy consumption of different electric vehicles, their battery capacity, and uncertainties arising from driving behavior or the environment, yet. Especially in the open source environment, nothing similar is to be found.
+                  </p>
+              <p>
+            GreenNav utilizes freely available OpenStreetMap geospatial data (osm.org) and NASA altitude maps. The front end is able to prototype different routing algorithms and to compare them.
           </p>
         </section>
       </div>
 
       <div style={{ background: "#1B5E20", color: "white" }}>
         <section style={STYLES.section}>
-          <h1>What is GreenNav</h1>
+          <h1>Participation in the Google Summer of Code 2016</h1>
           <p>
-            Green Navigation (abbreviated as GreenNav) provides driving directions for electric
-            vehicles and predicts their cruising range. It is a client-server system: The client
-            provides a platform independent web interface written in JavaScript. The server offers
-            access to specialized algorithms written in Java via a REST interface. The aim of the
-            project is to provide various tools for electric vehicles. There are two sides to the project:
-          </p>
-          <h2>For Drivers</h2>
-          <p>
-            On one side, we want to provide drivers of electric
-            vehicles with practical solutions, that is, with optimal driving direction for their route.
-            For that we are developing a platform independent navigation system.
-          </p>
-          <h2>For Scientists</h2>
-          <p>
-            On the other side, we
-            want to develop tools for scientifically analyzing algorithmic solutions. For that we are
-            developing tools for visualization of algorithms, for comparing their performance, we want
-            to provide a generic API for custom algorithms and a lot more.
+            GreenNav’s participation in the Google Summer of Code 2016 was a success for the project’s development. Thanks to the contributions, the front end is working properly. Still, we are still working on our back end for which everyone is invited to participate! Everything can be found on [GitHub](github.com/greennav).
+          </p><p>
+            Moreover, the project is published under the Apache License 2.0. This basically means that everyone is free to use and redistribute the software, but any original copyright, patent, trademark, and attribution notices must be preserved.
           </p>
           <img
             alt={config.siteTitle}
@@ -108,62 +79,24 @@ const Index = () => (
 
       <div style={{ background: "#F05D21", color: "white" }}>
         <section style={STYLES.section}>
-          <h1>Have a go!</h1>
+          <h1>Technologies used</h1>
           <p>
-            Try out GreenNav here, but notice that we are currently using only the map of Bavaria in Germany:
-          </p>
-          <p>
-            <a
-              href="http://www.isp.uni-luebeck.de/greennav"
-              style={{ color: "white" }}
-            >
-              http://www.isp.uni-luebeck.de/greennav <i className="fa fa-external-link" />
-            </a>
-          </p>
-          <p>
-            You could try to search for a route between Munich, Augsburg, Nuremberg, Regensburg and
-            other cities in Bavaria. Notice also, that some routes are not shown if the vehicles battery
-            charge is not sufficient. Here are some examples to try out:
-          </p>
-          <p>
-            <a
-              href="http://www.isp.uni-luebeck.de/greennav/?from=munich&to=augsburg&vehicle=Sam&battery=100"
-              style={{ color: "white" }}
-            >
-              Munich to Augsburg using a Sam with 100% battery <i className="fa fa-external-link" />
-            </a>
-          </p>
-          <p>
-            <a
-              href="http://www.isp.uni-luebeck.de/greennav/?from=nuremberg&to=erlangen&vehicle=Spyder-S&battery=100"
-              style={{ color: "white" }}
-            >
-              Nuremberg to Erlangen using a Spyder-S with 100% battery <i className="fa fa-external-link" />
-            </a>
+            Green Navigation makes use of
+            <ul>
+                <li>React Native for the front end</li>
+                <li>Java for the back end with</li>
+                <li>Spring for the REST-API and</li>
+                <li>Docker as a database container (WIP)</li>
+            </ul>
           </p>
         </section>
       </div>
 
       <div style={{ background: "#2C5770", color: "white"}}>
         <section style={STYLES.section}>
-          <h1>Background</h1>
+          <h1>Challenges</h1>
           <p>
-            The project was initiated by Martin Leucker and Martin Sachenbacher at the Technische
-            Universität München (TUM). After finishing a first prototype around 2009, the project
-            migrated to the University of Lübeck, where it is continued by René Schönfelder since
-            2012. Various ideas have since been implemented for scientific research within multiple
-            student projects. Overall there have been more than 50 students participating in the
-            development of the project.
-          </p>
-          <p>
-            While there are excellent routing services out there, we aim to support sustainable
-            means of transportation. One important aspect is therefore the combination of private
-            routes with public transportation. The routing problems arising are more complex than the
-            shortest path problem, the basic problem of finding a path with least costs in a road network.
-            We want to consider various aspects, such as a prediction of the energy consumption of
-            different electric vehicles, their battery capacity, uncertainties arising from driving
-            behavior or the environment, time-dependency as for public transportation or congested
-            routes, and a lot more.
+            One of the challenges is to develop a scheme for the database for more efficient queries. Also the development of a consumption model for electric cars and a faster implementation of routing algorithms needs to be worked on. This is a fantastic opportunity to derive the consumption using machine learning!
           </p>
         </section>
       </div>
